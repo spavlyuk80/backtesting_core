b25c72adc50bacc90317a10fd522456fdd6ee7af
Sergiy Pavlyuk <sergiypavlyuk@MacBook-Pro-2.local>
2020-07-01 07:42:55 +0300

    initial

---
+++ data.py
@@ -0,0 +1,11 @@
+class Data():
+    '''
+    This class maps general functions to broker's specific api's
+    '''
+    def __init__(self, params):
+        self.
+
+
+
+if __name__ == "__main__":
+    d = Data()
